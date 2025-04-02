ba86d145384852f7722e7d7d9185967a99b8e87c
William Wrolstad <inmind.wimw@gmail.com>
2025-04-02 14:38:30 -0500

    Added comments to make api-app/main.py more readable.

--- api-app/main.py
+++ api-app/main.py
@@ -1,30 +1,40 @@
 from fastapi import FastAPI, Query
 from Fibonacci import Fibonacci as fib
+
+#Iniitialize FastAPI
 app = FastAPI()
 
+#define api path
 @app.get("/api/fibonacci/{count}/")
 async def get_fib(count:int,one_line:bool=Query(default=False,alias="one-line"),last_only:bool=Query(default=False,alias="last-only"),numbering:bool=Query(default=False),help:bool=Query(default=False)):
     """Get {count} numbers of the fibonacci sequence"""
     output={}
+
+    #Get Fibonacci sequence
     sequence=fib.generate_fib(count)
     modified_sequence=sequence
+
+    #Number sequence if required
     if(numbering):
         for i in range(len(modified_sequence)):
             modified_sequence[i]=str(i+1)+":"+str(modified_sequence[i])
 
-
+    #build JSON if last-only is true
     if(last_only):
         output["last-number"]=modified_sequence[-1]
     else:
+        #Create comma-separated String for JSON output if required
         if(one_line):
             output_string=""
             for item in modified_sequence:
                 output_string+=str(item)+", "
             output_string=output_string.removesuffix(", ")
             output["sequence"]=output_string
+        #Use unchanged sequence if nothing else is required
         else:
             output["sequence"]=modified_sequence
 
+    #Add a help key to JSON response if requested
     if help:
         output["help"]="""Parameters
          count endpoint: /api/fibonacci/:count/ : Calculate to this many places. IE: 0,1,1,2,3,5 would be the result of /api/fibonacci/6
@@ -37,5 +47,5 @@ async def get_fib(count:int,one_line:bool=Query(default=False,alias="one-line"),
 
          See docs at /docs
          """
-
+    #outputs final JSON string
     return output
